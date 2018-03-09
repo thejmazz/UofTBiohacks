41407cc15cfe70f5d42623cbe9a4d2f79fe70c39
ayalaemmylou <ayalaemmylou@gmail.com>
2018-03-08 22:26:09 -0500

    Update timeline.js
    Updated speaker information and added some overnight events

--- static/src/assets/js/timeline.js
+++ static/src/assets/js/timeline.js
@@ -29,43 +29,12 @@ export default [
     'icon': 'fa fa-fire',
     'content':
     `
-      <p>
-        SS2135
-      </p>
-    `
-  },
-  {
-    'time': '10:30 a.m.',
-    'title': 'Hacking Begins!',
-    'icon': 'fa fa-code',
-    'content':
-    `
-      <p>
-        Get comfy and Start Hacking! Mentors will be circulating to answer questions!
-      </p>
-    `
-  },
-  {
-    'time': '12:00 p.m.',
-    'title': 'Lunch',
-    'icon': 'fa fa-cutlery',
-    'content':
-    `
-      <p>
-        Sidney Smith
-      </p>
-    `
-  },
-  {
-    'time': '1:00 p.m.',
-    'title': 'Guest Speaker: Prof. Nicholas Provart',
-    'icon': 'fa fa-microphone',
-    'content':
-    `
       <h3>
         SS2135
-        Raising the BAR for Visual Analytics: Building Tools for Hypothesis Generation with Open Big Data
       </h3>
+      <h4>
+        Prof. Nicholas Provart on Raising the BAR for Visual Analytics: Building Tools for Hypothesis Generation with Open Big Data
+      </h4>
       <p>
         Nicholas Provart is a full professor of Plant Cyberinfrastructure and Systems Biology in
         the Department of Cell &amp; Systems Biology at the University of Toronto. Currently his
@@ -81,6 +50,45 @@ export default [
       <div class="headshot-wrapper">
         <img class="headshot" src="/img/Provart_Nov2015_585x585px.png"/>
       </div>
+      <h4>
+        Prof. Boris Steipe to Introduce the Challenge and Starter Code
+      </h4>
+      <p>
+        Boris Steipe - 
+        MD PhD Ludwig Maximilian's University, Munich, Germany;
+        Postdoc in protein structure with Robert Huber, Max Planck Institute for Biochemistry; 
+        Habilitation in Biochemistry; 
+        Junior research fellow (protein engineering), Gene Center of the University, Munich; 
+        Associate professor, Department of Biochemistry and Department of Molecular Genetics, University of Toronto.
+        Director, Undergraduate Specialist Program in Bioinformatics and Computational Biology; 
+        Linguistics Specialist POSt (part time).
+      </p>
+      <div class="headshot-wrapper">
+        <img class="headshot" src="/img/BorisSteipeHeadshot.jpg"/>
+      </div>
+    `
+  },
+  {
+    'time': '10:30 a.m.',
+    'title': 'Hacking Begins!',
+    'icon': 'fa fa-code',
+    'content':
+    `
+      <p>
+        Get comfy and Start Hacking in Rooms SS 2104/2105/2106/2108/2112/2114/2116/2119/1084/1086!
+        Mentors will be circulating to answer questions and help you get started!
+      </p>
+    `
+  },
+  {
+    'time': '12:00 p.m.',
+    'title': 'Lunch',
+    'icon': 'fa fa-cutlery',
+    'content':
+    `
+      <p>
+        Sidney Smith
+      </p>
     `
   },
   {
@@ -89,22 +97,60 @@ export default [
     'icon': 'fa fa-microphone',
     'content':
     `
+      <h4>
+        Computational approaches to correction of a splicing defect in a mouse model of congenital muscular dystrophy 
+      </h4>
       <p>
-        SS2135
+        Amit Deshwar is a research scientist at Deep Genomics.  His doctoral work was at the
+        University of Toronto under Quaid Morris using Bayesian Non-parametrics to study intra-tumour
+        heterogeneity and evolution.  He is a Vanier Scholar and former Junior Fellow at Massey College. 
+        Previously he worked at Google, started two companies and  obtained undergraduate degrees in Software
+        Engineering and Psychology.
       </p>
+      <div class="headshot-wrapper">
+        <img class="headshot" src="/img/AmitDeswarHeadshot.jpg"/>
+      </div>
     `
   },
   {
     'time': '4:00 p.m.',
-    'title': 'CAGEF Workshop', 
+    'title': 'Guest Speaker: Erica Acton from CAGEF', 
     // TODO update font awesome
+    // No Need to change the icon as this is now a talk and we can use microphone
     // 'icon': 'fa fa-dna',
-    'icon': 'fa fa-wrench',
+    'icon': 'fa fa-microphone',
     'content':
     `
-      <p>
+      <h3>
         SS2135
+      </h3>
+      <h4>
+        Metagenomics Analysis: What's in your Microbial Goody Bag?
+      </h4>
+
+      <p>
+        Erica Acton entered the field of data science when she needed to 'get stuff done' during her Master's degree
+        in Genome Science and Technology at the University of British Columbia. She has subsequently worked as a bioinformatician
+        in Vancouver and Toronto, analyzing transcriptome data and visualizing large genomics datasets. She sometimes leaves her 
+        keyboard to give talks or train others at science education and outreach events that promote data science, or to run
+        half-marathons. She is currently at the Centre for the Analysis of Genome Evolution and Function (CAGEF) where she is
+        creating bioinformatics training lessons and workshops, and drinking copious amounts of coffee.
       </p>
+      <div class="headshot-wrapper">
+        <img class="headshot" src="/img/EricaActonHeadshot.jpeg"/>
+      </div>
+      <p>
+        The Centre for the Analysis of Genome Evolution and Function (CAGEF) promotes interdisciplinary research in
+        comparative, evolutionary, and functional analyses of genomes and proteomes, and to promote training and 
+        education in genome biology through the development and support of innovative teaching initiatives, courses,
+        and workshops. CAGEF provides genomics, proteomics, and bioinformatic services, including Illumina-based genome,
+        microbiome, and metagenome sequencing and transcriptome analysis, and proteome and protein modification analysis
+        via LC-MS/MS. CAGEF has genomics expertise in the analysis of non-standard species, systems, and environments, with
+        particular experience in microbial, plant, and environmental genomics and metagenomics.
+      </p>
+      <div class="headshot-wrapper">
+        <img class="headshot" src="/img/CAGEFLogo.png"/>
+      </div>
     `
   },
   {
@@ -115,6 +161,29 @@ export default [
     `
       <p>
         Sidney Smith
+      </p>
+    `
+  },
+  {
+    'time': '10:00 p.m.',
+    'title': 'Cup Stacking',
+    //If you can think of a better icon go for it
+    'icon': 'fa fa-bolt',
+    'content':
+    `
+      <p>
+        Location: TBD
+      </p>
+    `
+  },
+  {
+    'time': '12:00 p.m.',
+    'title': 'Midnight Games',
+    'icon': 'fa fa-gamepad',
+    'content':
+    `
+      <p>
+        SS2135
       </p>
     `
   },
